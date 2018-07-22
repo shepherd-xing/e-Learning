443d0eabad371562c9253bd879d07a63bf65438c
xing <1741807421@qq.com>
2018-07-22 16:49:56 +0800

    为不同内容创建模型

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 class Subject(models.Model):
     title = models.CharField('标题', max_length=200)
@@ -24,10 +26,47 @@ class Course(models.Model):
 class Module(models.Model):
     '''课程单元'''
     course = models.ForeignKey(Course, verbose_name='课程', related_name='modules')
-    title = models.CharField('标签', max_length=200)
+    title = models.CharField('标题', max_length=200)
     description = models.TextField('描述', blank=True)
     def __str__(self):
         return self.title
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, verbose_name='课程单元', related_name='contents')
+    content_type = models.ForeignKey(ContentType, verbose_name='内容类型',
+                                     limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
+    object_id = models.PositiveIntegerField('关联模型的主键')
+    item = GenericForeignKey('content_type', 'object_id')
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, verbose_name='教师', related_name='%(class)s_related')
+    title = models.CharField('标题', max_length=250)
+    created = models.DateTimeField('创建时间', auto_now_add=True)
+    updated = models.DateTimeField('修改时间', auto_now=True)
+
+    class Meta:
+        abstract = True
+    def __str__(self):
+        return self.title
+
+class Text(ItemBase):
+    content = models.TextField('文本内容')
+
+class File(ItemBase):
+    file = models.FileField('文件', upload_to='files')
+
+class Image(ItemBase):
+    file = models.FileField('图片', upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField('视频URL')
+
+
+
+
+
+
+
 
 
 
