ca39de663f2a5e4327b2cfffed3431a1a347be01
xing <1741807421@qq.com>
2018-07-23 17:17:14 +0800

    限制访问基于类的视图

--- courses/views.py
+++ courses/views.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Course
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin
 
 # Create your views here.
 class OwnerMixin():
@@ -14,10 +15,12 @@ class OwnerMixin():
 class OwnerEditMixin():
     def form_valid(self, form):
         form.instance.owner = self.request.user         #保存对象时设置当前用户
-        return super.form_valid(form)
+        return super().form_valid(form)
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
     model = Course      #用于queryset的模型，被所有视图使用
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     fields = ['subject', 'title', 'slug', 'overview']       #构建CreateView和UpdateView视图的模型表单
@@ -28,15 +31,17 @@ class ManageCourseListView(OwnerCourseMixin, ListView):
     '''列出用户创建的课程'''
     template_name = 'courses/manage/course/list.html'
 
-class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
 
-class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
+    template_name = 'courses/manage/course/form.html'
+    permission_required = 'courses.change_course'
 
-class CourseDeleteView(OwnerCourseMixin, DeleteView):
+class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')        #删除后重定向
+    permission_required = 'courses.delete_course'
 
 
 
