30e21e8b10c1a63b6136c52b5bec6e8f28b135cd
xing <1741807421@qq.com>
2018-07-24 19:03:22 +0800

    重新排序单元和内容

--- courses/urls.py
+++ courses/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
         views.ContentCreateUpdateView.as_view(), name='module_content_update'),
     url(r'^content/(?P<id>\d+)/delete/$', views.ContentDeleteView.as_view(), name='module_content_delete'),
     url(r'^module/(?P<module_id>\d+)/$', views.ModuleContentListView.as_view(), name='module_content_list'),
+    url(r'^module/order/$', views.ModuleOrderView.as_view(), name='module_order'),
+    url(r'^content/order/$', views.ContentOrderView.as_view(), name='content_order'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -4,7 +4,7 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateResponseMixin, View
 from .models import Course, Module, Content
-from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin, CsrfExemptMixin, JsonRequestResponseMixin
 from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
@@ -116,7 +116,17 @@ class ModuleContentListView(TemplateResponseMixin, View):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response({'module':module})
 
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved':'OK'})
 
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved':'OK'})
 
 
 
