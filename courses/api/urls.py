253de9d5f41952815a6212ce29761d76f68e3c3d
xing <1741807421@qq.com>
2018-07-27 13:12:56 +0800

    添加权限

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^subjects/$', views.SubjectListView.as_view(), name='subject_list'),
-    url(r'^subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='subject_detail')
+    url(r'^subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='subject_detail'),
+    url(r'^courses/(?P<pk>\d+)/enroll/$', views.CourseEnrollView.as_view(), name='course_enroll'),
 ]
